905bd02a3defbfb0d0b02505f5ae7feecaffe565
mameharu2020 <soayamay@gmail.com>
2024-04-14 18:19:30 +0900

    デプロイの準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
-
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == 'administrator' && password == '1111'
     end
   end
 end
