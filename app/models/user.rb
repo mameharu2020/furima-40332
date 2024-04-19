281e934be1429801b90533b55f9692416d18dc69
mameharu2020 <soayamay@gmail.com>
2024-04-19 22:14:16 +0900

    アソシエーションを削除、バリデーションの正規表現を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 
   validates :nickname, presence: true
-  validates :password, :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/, message: '英数字混合で入力してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英数字混合で入力してください' }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
