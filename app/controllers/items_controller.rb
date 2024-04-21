092fb9a026f073ee29af1d4eab67a2d52ee99c92
mameharu2020 <soayamay@gmail.com>
2024-04-22 00:35:56 +0900

    データの登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,32 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
+  #before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @items = Item.all
   end
 
-  def move_to_index
-    return if user_signed_in?
-
-    redirect_to action: :index
+  def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @items = Item.includes(:user)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:shipping_cost_id,:prefecture_id,:shipping_date_id,:price).merge(user_id: current_user.id)
+  end
+
+  #def move_to_index
+    #redirect_to root_path unless user_signed_in?
+  #end
+  
 end
