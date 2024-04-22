4da8badd8b6f5fa6aa69739cc64e5d10fd17e72e
mameharu2020 <soayamay@gmail.com>
2024-04-22 13:21:51 +0900

    モデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.email }
-    password { 'aaa111' }
+    password { Faker::Internet.password(min_length: 6, max_length: 129, mix_case: true) }
     password_confirmation { password }
     family_name { '山田' }
     first_name { '太郎' }
     family_name_kana { 'ヤマダ' }
     first_name_kana { 'タロウ' }
-    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    birthday { Faker::Date.birthday(min_age: 18) }
   end
 end
