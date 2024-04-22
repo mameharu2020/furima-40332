597877425e0b1559b06922825abc8520a2cab9ba
mameharu2020 <soayamay@gmail.com>
2024-04-22 20:37:49 +0900

    金額入力時に販売手数料や販売利益の計算結果を表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById('item-price');
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const feeOutput = document.getElementById('add-tax-price');
+    const addTaxDom = document.getElementById("profit");
+
+    const fee = Math.floor(inputValue * 0.1)
+    const profit = Math.floor(inputValue - fee)
+
+    feeOutput.innerHTML = fee.toLocaleString();
+    addTaxDom.innerHTML = profit.toLocaleString();
+  })
+ })
