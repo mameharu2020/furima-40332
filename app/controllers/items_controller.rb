b8df540f40fd71e0f67d3581d2b2fdce57e88c2e
mameharu2020 <soayamay@gmail.com>
2024-04-26 22:22:28 +0900

    編集作業をもとに、DBの内容を更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,12 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  end
   private
   def item_params
     params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:shipping_cost_id,:prefecture_id,:shipping_date_id,:price).merge(user_id: current_user.id)
