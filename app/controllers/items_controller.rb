8d32409ae7e49da980099ac414cf4e0ab616dce2
mameharu2020 <soayamay@gmail.com>
2024-04-27 18:35:40 +0900

    出品者本人かそれ以外かの条件分岐、エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  
+  before_action :move_to_new_user_session, only: [:edit]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -25,15 +26,26 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless @item.user == current_user
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:shipping_cost_id,:prefecture_id,:shipping_date_id,:price).merge(user_id: current_user.id)
+  end
+
+  def move_to_new_user_session
+    unless current_user
+      redirect_to new_user_session_path unless user_signed_in?
+    end
   end
 end
