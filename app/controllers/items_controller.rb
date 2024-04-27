3060b15fb3ab416925434034ba73d568e7e3dd3e
mameharu2020 <soayamay@gmail.com>
2024-04-27 18:42:59 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_new_user_session, only: [:edit]
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
@@ -39,13 +39,15 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:image,:name,:description,:category_id,:status_id,:shipping_cost_id,:prefecture_id,:shipping_date_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_new_user_session
-    unless current_user
-      redirect_to new_user_session_path unless user_signed_in?
-    end
+    return if current_user
+
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -24,5 +24,4 @@ class Category < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :items
-
 end
