8dd14182cc0262153df2b85ff0ad5bab29c173d5
mameharu2020 <soayamay@gmail.com>
2024-05-05 23:30:00 +0900

    購入のためのフォームを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,18 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @order_shipping_address = OrderShippingAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+
   end
+
+  #private
+
+  #def メソッド名
+    #params.permit(指定のカラムを記述する)
+  #end
 
 end
