0134a16735432977460172fa7d743344347c77f0
mameharu2020 <soayamay@gmail.com>
2024-05-06 17:14:14 +0900

    ActionView::Template::Errorの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_shipping_address = OrderShippingAddress.new 
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)    
+    @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
     if @order_shipping_address.valid? 
       @order_shipping_address.save
       redirect_to root_path
@@ -19,5 +19,9 @@ class OrdersController < ApplicationController
 
   def order_shipping_address_params
     params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
