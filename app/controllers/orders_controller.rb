5b97bb28e530dbf08c10b4675e2dd3ca61a8815c
mameharu2020 <soayamay@gmail.com>
2024-05-06 16:50:53 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
-    @order_shipping_address = OrderShippingAddress.new
+    @order_shipping_address = OrderShippingAddress.new 
     @item = Item.find(params[:item_id])
   end
 
   def create
-
+    @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)    
+    if @order_shipping_address.valid? 
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render 'orders/index', status: :unprocessable_entity
+    end
   end
 
-  #private
+  private
 
-  #def メソッド名
-    #params.permit(指定のカラムを記述する)
-  #end
-
+  def order_shipping_address_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,6 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "should be 10 or 11 digits without hyphens" }
@@ -12,7 +12,7 @@ class OrderShippingAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, order_id: order.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
   
 end
