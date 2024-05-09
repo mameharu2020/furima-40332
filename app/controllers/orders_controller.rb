6781dd9725f021d40a7e879fdec28cbc445f6462
mameharu2020 <soayamay@gmail.com>
2024-05-10 00:41:55 +0900

    クレジットカード決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,19 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
-
+  
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping_address = OrderShippingAddress.new 
   end
 
   def create
     @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
-    if @order_shipping_address.valid? 
+    if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'orders/index', status: :unprocessable_entity
     end
   end
@@ -18,10 +21,19 @@ class OrdersController < ApplicationController
   private
 
   def order_shipping_address_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_shipping_address_params[:token],
+        currency: 'jpy'
+      )
   end
 end
