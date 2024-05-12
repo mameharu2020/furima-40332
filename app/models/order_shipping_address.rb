3aa212f8c6b87d42e9c01605cbf1645d7676a097
mameharu2020 <soayamay@gmail.com>
2024-05-12 20:33:27 +0900

    Stashを永続化

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,5 @@
 class OrderShippingAddress
   include ActiveModel::Model
-<<<<<<< Updated upstream
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
@@ -18,23 +17,4 @@ class OrderShippingAddress
     order = Order.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
-=======
-  attr_accessor :postal_code, :prefecture, :city, :addresses, :building, :user_id, :item_id, :created_at
-
-  with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-    validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-
-  def save
-    # 寄付情報を保存し、変数donationに代入する
-    donation = Donation.create(price: price, user_id: user_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
-  end
-  
->>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-<<<<<<< Updated upstream
     resources :orders, only: [:index, :create]
-=======
-    resources :orders, only:[:index, :create]
->>>>>>> Stashed changes
   end
 end
