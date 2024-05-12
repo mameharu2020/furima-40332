f21d71a3251c11b7e5cc81beb170efdf6759425b
mameharu2020 <soayamay@gmail.com>
2024-05-12 20:12:31 +0900

    バリデーションをグループ化

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -2,14 +2,16 @@ class OrderShippingAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
-    validates :postal_code  , presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city         , presence: true
-    validates :addresses    , presence: true
-    validates :phone_number , presence: true, format: { with: /\A\d{10,11}\z/, message: "should be 10 or 11 digits without hyphens" }
-    validates :user_id      , presence: true
-    validates :item_id      , presence: true
-    validates :token        , presence: true
+  with_options presence: true do
+    validates :postal_code  , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number , format: { with: /\A\d{10,11}\z/, message: "should be 10 or 11 digits without hyphens" }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
