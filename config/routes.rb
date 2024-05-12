b39f0ceeff90ac9fd33f8633ab8fed9fe16f3f96
mameharu2020 <soayamay@gmail.com>
2024-05-12 20:31:36 +0900

    Stashを永続化

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
+<<<<<<< Updated upstream
     resources :orders, only: [:index, :create]
+=======
+    resources :orders, only:[:index, :create]
+>>>>>>> Stashed changes
   end
 end
